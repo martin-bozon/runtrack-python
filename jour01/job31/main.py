801a796d0f3aa6b48e2788412660c353273170ac
Martin Bozon <62135515+martin-bozon@users.noreply.github.com>
2021-11-02 16:12:33 +0100

    job 31 fait

--- jour01/job31/main.py
+++ jour01/job31/main.py
@@ -1,13 +1,16 @@
-def test(note):
-    if note > 40:
-        x = 0
-        while x < 4:
-            if (note + x) % 5 == 0:
-                print('done', note + x)
-        x += 1
+def test(notes):
+    new_notes = []
+    for note in notes:
+        if note > 40:
+            x = 0
+            while x < 3:
+                if (note + x) % 5 == 0:
+                    note = note + x
+                x += 1
+            new_notes.append(note)
+        else:
+            new_notes.append(note)
+    print(new_notes)
 
-    else:
-        print('fail:', note)
 
-
-test(83)
+test([83, 82])
