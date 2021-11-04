67ae612693bb5f79cbf3470441e28811ce9f0eaa
Martin Bozon <62135515+martin-bozon@users.noreply.github.com>
2021-11-04 14:30:26 +0100

    job 03 fait

--- jour03/job03/main.py
+++ jour03/job03/main.py
@@ -1,6 +1,6 @@
 import re
 
-choice = input("Choisis un entier:")
+choice = int (input("Choisis un entier:"))
 count = 0
 
 f = open("data.txt", "r")
@@ -11,10 +11,10 @@ f.close()
 for line in lines:
     for mot in line.split(" "):
         wordwhitoutpunctuation = re.sub(r'[^\w\s]', '', mot)
-        m = re.search(r'(\S[a-zA-Z]*\S)', wordwhitoutpunctuation)
-        if m != None and m=longueurchoix:
-            print(m)
-            count += 1
+        if len(wordwhitoutpunctuation) == choice:
+            m = re.search(r'(\S[a-zA-Z]*\S)', wordwhitoutpunctuation)
+            if m != None:
+                count += 1
 
-# r'(\S[a-zA-Z]*\S{'+choice+'})'
+
 print(count)
